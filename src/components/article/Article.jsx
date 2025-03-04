649e8bf70aa358cac094c93f49a4fa2b23d96abe
copiates <kirthan783@gmail.com>
2025-03-04 16:25:16 +0530

    k

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './article.css'
+import whatsappImage from '../../assets/whatsapp2.jpg'
 
 const Article = () => {
   return (
     <div className="article-container">
-      <h1>Article</h1>
       <a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" />
+        <img src={whatsappImage} alt="WhatsApp" />
       </a>
     </div>
   )
