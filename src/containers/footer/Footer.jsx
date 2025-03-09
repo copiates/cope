42f3b04f9c830e1e40f54493d8693f510af8d5e6
copiates <kirthan783@gmail.com>
2025-03-09 12:33:51 +0530

    done frr

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -10,11 +10,11 @@ const Footer = () => {
             <p>Phone: +91 9845643599</p>
             <p>Email: caamaranath@yahoo.com</p>
             <p>Timings: Monday - Saturday , 10AM - 6PM</p>
-            <p>No 3440,G-1, SR Harmony, 4th Cross, 10th Main Rd, Indiranagar, Bengaluru-560038</p>
+            <p>No 3440, G-1, SR Harmony, 4th Cross, 10th Main Rd, Indiranagar, Bengaluru-560038</p>
           </div>
       </div>
       <div className="footer-copyright">
-        <p>© 2025 B V Swami & Co, Chartered Accountants</p>
+        <p>© 2025 B.V. Swami & Co, Chartered Accountants</p>
       </div>
     </div>
   )
