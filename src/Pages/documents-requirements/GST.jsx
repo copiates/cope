47a804aab241f1842c985aef1ec35d61e555a961
copiates <kirthan783@gmail.com>
2025-03-10 23:36:16 +0530

    dosa

--- src/Pages/documents-requirements/GST.jsx
+++ src/Pages/documents-requirements/GST.jsx
@@ -44,7 +44,7 @@ const GST = () => {
               <li>PAN card and Aadhar card of authorized signatory. The authorised signatory must be an Indian even in case of foreign companies/branch registration</li>
             </ul>
 
-            <h3>Documents required for GST registration based on the nature of business activity:</h3>
+            <h4>Documents required for GST registration based on the nature of business activity:</h4>
 
             <h4>Normal taxpayer Registration (including composition dealer, government departments and ISD registrations)</h4>
             <ul>
